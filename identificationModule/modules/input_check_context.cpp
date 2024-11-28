48297007308cda031bd05d3128c75eac47be1574
FireFox <115025745+20FireFox02@users.noreply.github.com>
2024-11-28 17:58:55 +0300

    space_corrections

--- identificationModule/modules/input_check_context.cpp
+++ identificationModule/modules/input_check_context.cpp
@@ -1,7 +1,7 @@
 #include "input_check_context.hpp"
 
 bool InputCheckContex::ElementCheck(std::string active_agent, ScAddr element) {
-    SC_LOG_INFO(active_agent<<":"<<"Element check");
+    SC_LOG_INFO(active_agent<<": "<<"Element check");
     if (!IsElement(element)) {
         SC_LOG_ERROR(active_agent<<":"<<"Element is not specified.");
         return false;
@@ -10,7 +10,7 @@ bool InputCheckContex::ElementCheck(std::string active_agent, ScAddr element) {
 }
 
 bool InputCheckContex::EmptyCheck(std::string active_agent, ScAddr input_field, std::string * input_field_str) {
-    SC_LOG_INFO(active_agent<<":"<<"Empty check");
+    SC_LOG_INFO(active_agent<<": "<<"Empty check");
     if (!GetLinkContent(input_field, (*input_field_str))) {
         SC_LOG_ERROR(active_agent<<":"<<"Field is empty.");
         return false;
@@ -19,16 +19,16 @@ bool InputCheckContex::EmptyCheck(std::string active_agent, ScAddr input_field, 
 }
 
 bool InputCheckContex::SpaceCheck(std::string active_agent, std::string & str) {
-    SC_LOG_INFO(active_agent<<":"<<"Space check");
+    SC_LOG_INFO(active_agent<<": "<<"Space check");
     for (char i : str) if (i == ' ') {
-        SC_LOG_ERROR(active_agent<<":"<<"You can't enter a space");
+        SC_LOG_ERROR(active_agent<<": "<<"You can't enter a space");
         return false;
     }
     return true;
 }
 
 bool InputCheckContex::SizeCheck(std::string active_agent, std::string & str) {
-    SC_LOG_INFO(active_agent<<":"<<"Size check");
+    SC_LOG_INFO(active_agent<<": "<<"Size check");
     if (str.size() < 8) {
         SC_LOG_ERROR(active_agent<<":"<<"String size cannot be less than 8 symbols.");
         return false;
