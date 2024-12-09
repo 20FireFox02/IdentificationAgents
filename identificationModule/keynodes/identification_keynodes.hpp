48fd402e0d0312f591fb1148e566ca290dcfa50f
FireFox <115025745+20FireFox02@users.noreply.github.com>
2024-12-09 18:42:46 +0300

    new_keynodes

--- identificationModule/keynodes/identification_keynodes.hpp
+++ identificationModule/keynodes/identification_keynodes.hpp
@@ -7,9 +7,10 @@ class IdentificationKeynodes : public ScKeynodes
 public:
     static inline ScKeynode const action_authorization{"action_authorization", ScType::ConstNodeClass};
     static inline ScKeynode const action_registration{"action_registration", ScType::ConstNodeClass};
-    static inline ScKeynode const user{"user", ScType::ConstNodeClass};
-    static inline ScKeynode const login{"login", ScType::ConstNodeClass};
-    static inline ScKeynode const password{"password", ScType::ConstNodeClass};
+    static inline ScKeynode const concept_user{"concept_user", ScType::ConstNodeClass};
+    static inline ScKeynode const concept_login{"concept_login", ScType::ConstNodeClass};
+    static inline ScKeynode const concept_password{"concept_password", ScType::ConstNodeClass};
     static inline ScKeynode const nrel_login{"nrel_login", ScType::ConstNodeNonRole};
     static inline ScKeynode const nrel_password{"nrel_password", ScType::ConstNodeNonRole};
+    static inline ScKeynode const nrel_user_idtf{"nrel_user_idtf", ScType::ConstNodeNonRole};
 };
